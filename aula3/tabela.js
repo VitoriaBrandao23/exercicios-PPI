34485ea08c2c2064f78dced036dc39270668cdef
Vitória Beatriz Batista Brandão <132864073+VitoriaBrandao23@users.noreply.github.com>
2025-04-23 11:43:28 -0300

    sala ensaio

---
+++ aula3/tabela.js
@@ -0,0 +1,11 @@
+function tabela(){
+    var linhas = document.getElementById('linhas');
+    var colunas = document.getElementById('colunas');
+
+    var nLinhas = linhas.value;
+    var nColunas = colunas.value;
+
+    
+
+    return nLinhas, nColunas;
+}
